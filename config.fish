e3a0ca2ed76bd0bd17125807f88b552691276f86
Edoardo Ranghieri <edoardo@ranghieri.com>
2021-07-08 03:00:07 +0200

    Add Starship configuration and auto-install

--- config.fish
+++ config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if not type -q starship
+  curl -fsSL https://starship.rs/install.sh | bash
+end
+
+set -Ux STARSHIP_CONFIG "$HOME/.config/fish/starship.toml"
 starship init fish | source
